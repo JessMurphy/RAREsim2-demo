bc70eabb8cb0f2246a06012fe528bc5b18e1c86b
Jessica Murphy <jessica.murphy@cuanschutz.edu>
2026-01-27 16:22:03 -0700

    Update 5_submit_results.sh

--- 5_submit_results.sh
+++ 5_submit_results.sh
@@ -6,5 +6,8 @@
 #SBATCH -p math-alderaan
 #SBATCH --mem-per-cpu=4096MB
 
-# 5. Plot results
+# a. Plot power and type I error results
 singularity exec /storage/singularity/mixtures.sif Rscript ./5a_plot_results.R
+
+# b. Plot allele frequency distribution (AFS) info for the target (gnomad) and simulated data
+singularity exec /storage/singularity/mixtures.sif Rscript ./5b_plot_afs.R
